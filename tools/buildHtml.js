aa0f9549c3c4a8938712c71a192feef7771f480f
reza-abdollahi <abdollahi.reza88@gmail.com>
2019-06-23 12:27:50 +0430

    webpack production build

---
+++ tools/buildHtml.js
@@ -0,0 +1,24 @@
+// This script copies src/index.html into /dist/index.html
+import fs from 'fs';
+import cheerio from 'cheerio';
+import colors from 'colors';
+
+/*eslint-disable no-console */
+
+fs.readFile('src/index.html', 'utf8', (err, markup) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  const $ = cheerio.load(markup);
+
+  // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
+  $('head').prepend('<link rel="stylesheet" href="styles.css">');
+
+  fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('index.html written to /dist'.green);
+  });
+});
