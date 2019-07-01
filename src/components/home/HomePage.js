2270e78cbb15c9d136ec477776e23ca4902fdafc
reza-abdollahi <abdollahi.reza88@gmail.com>
2019-07-01 10:37:33 +0430

    rename line directory to lowercase

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getAllLines} from '../../actions/lineActions';
-import SelectLine from '../Line/SelectLine';
+import SelectLine from '../line/SelectLine';
 
 class HomePage extends React.Component {
   componentDidMount(){
