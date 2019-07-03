0abb7ec861b99ab1ff53d02e6f06c7f9488f696e
reza-abdollahi <abdollahi.reza88@gmail.com>
2019-07-03 16:32:39 +0430

    add redux-logger to log the actions and state changes in the console

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,12 @@ export default function configureStore(initialState) {
   const middleware = [thunk];
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(require('redux-immutable-state-invariant')());
+
+    const { createLogger } = require(`redux-logger`);
+    const logger = createLogger({
+      collapsed: (getState, action, logEntry) => !logEntry.error
+    });
+    middleware.push(logger);
   }
 
   return createStore(
